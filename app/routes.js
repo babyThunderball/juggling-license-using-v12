f53eb868275c19aac401e5ab47c69063a8c89004
Jodi <akajezebel@gmail.com>
2022-01-07 17:31:03 +0000

    Added Vicky Teinaki's code to app/config.js, app/route.js, and server.js.

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// GET SPRINT NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+    res.locals.currentURL = req.originalUrl; // current screen
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+
+    console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );
+
+    next()
+})
 // Run this code when a form is submitted to 'juggling-balls-answer'
 router.post('/juggling-balls-answer', function (req, res) {
 
